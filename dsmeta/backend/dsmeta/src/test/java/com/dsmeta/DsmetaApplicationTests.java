31b8244cef907affb74a97bc08467f63cda168ae
vitoriaacarvalho <vitoriabcarvalhocorrea@gmail.com>
2022-09-08 19:05:11 -0300

    frontend done!

---
+++ dsmeta/backend/dsmeta/src/test/java/com/dsmeta/DsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- dsmeta/frontend/frontend/src/components/SalesCard/index.tsx
+++ dsmeta/frontend/frontend/src/components/SalesCard/index.tsx
@@ -1,15 +1,35 @@
 import './styles.css'
+import { useState } from 'react';
 import NotificationButton from '../NotificationButton';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
 function SalesCard(){
+
+  const min=new Date(new Date().setDate(new Date().getDate()-365));
+  const max=new Date();
+  const [minDate, setMinDate] = useState(min);
+  const [maxDate, setMaxDate]= useState(max);
     return(
           <div className="dsmeta-card">
             <h2 className="dsmeta-sales-title">Vendas</h2>
             <div>
               <div className="dsmeta-form-control-container">
-                <input className="dsmeta-form-control" type="text"/>
+                <DatePicker
+                  selected={minDate}
+                  onChange={(date: Date)=> setMinDate(date)}
+                  className="dsmeta-form-control"
+                  dateFormat='dd/MM/yyyy'
+                />
+
               </div>
               <div className="dsmeta-form-control-container">
-                <input className="dsmeta-form-control" type="text"/>
+              <DatePicker
+                  selected={maxDate}
+                  onChange={(date: Date)=> setMaxDate(date)}
+                  className="dsmeta-form-control"
+                  dateFormat='dd/MM/yyyy'
+                />
               </div>
             </div>
 
